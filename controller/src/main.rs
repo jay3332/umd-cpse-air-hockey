f20d82a1736dfa883812c454149616a2b6110340
jay3332 <40323796+jay3332@users.noreply.github.com>
2024-11-13 09:41:26 -0500

    use a buffering read for usart

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -6,13 +6,13 @@
 mod stepper;
 use stepper::Stepper;
 
-use arduino_hal::prelude::_embedded_hal_serial_Read;
 use arduino_hal::{
     hal::{
-        usart::{BaudrateArduinoExt, UsartOps},
-        Atmega, Pins as SrcPins,
+        usart::{BaudrateArduinoExt, Usart0},
+        Pins as SrcPins,
     },
-    Peripherals, Pins, Usart,
+    prelude::*,
+    DefaultClock, Peripherals, Pins,
 };
 #[allow(unused_imports)]
 use panic_halt;
@@ -21,42 +21,133 @@ use panic_halt;
 /// Current value taken from <https://github.com/jjrobots/Air_Hockey_Robot_EVO/blob/master/Arduino/AHRobot_EVO/AHRobot_EVO.ino#L38>
 const BITRATE: u32 = 115_200;
 
-/// Maximum motor speed, in revolutions per minute.
-const MOTOR_MAX_SPEED: i32 = 32_000;
+type Serial = Usart0<DefaultClock>;
 
-trait SerialExt {
-    fn read_i32(&mut self) -> i32;
+pub struct Hardware {
+    pub serial: Serial,
+    pub stepper_a: Stepper,
+    pub stepper_b: Stepper,
 }
 
-impl<U: UsartOps<Atmega, Rx, Tx>, Rx, Tx> SerialExt for Usart<U, Rx, Tx> {
-    fn read_i32(&mut self) -> i32 {
-        i32::from_le_bytes([self.read_byte(); 4])
+impl Hardware {
+    /// Maximum motor speed, in revolutions per minute.
+    const MOTOR_MAX_SPEED: i32 = 32_000;
+
+    /// Computes the relative velocity of the paddle (X, Y), where X and Y are in the range [-1, 1].
+    /// The speed is relative to the maximum speed of the motor.
+    #[inline]
+    const fn corexy_to_cartesian(mut x: f32, mut y: f32) -> (f32, f32) {
+        // If x + y > 1, scale the values such that x + y = 1.
+        if x + y > 1.0 {
+            let scale = 1.0 / (x + y);
+            x *= scale;
+            y *= scale;
+        }
+
+        // Define the two stepper motors as A and B, turning in the X direction clockwise.
+        // * If A = -X and B = X, the paddle moves kX in the +Y direction (where k is a constant)
+        // * If A = B = X, the paddle moves kX in the -X direction
+        //
+        // The speed of the motors is defined as:
+        // * A = (X + Y) * MAX_SPEED
+        // * B = (X - Y) * MAX_SPEED
+        //
+        // Reference: <http://wiki.fluidnc.com/en/config/kinematics>
+        let a = (x + y) * Self::MOTOR_MAX_SPEED as f32;
+        let b = (x - y) * Self::MOTOR_MAX_SPEED as f32;
+        (a, b)
+    }
+
+    /// Runs the `V` command to set the relative speed of the paddle.
+    ///
+    /// # Parameters
+    /// - `x`: The relative speed of the paddle in the X direction in the range [-10^4, 10^4].
+    /// - `y`: The relative speed of the paddle in the Y direction in the range [-10^4, 10^4].
+    pub fn set_rel_speed(&mut self, x: i16, y: i16) {
+        const RANGE: i16 = 10_000;
+        let (a, b) = Self::corexy_to_cartesian(x as f32 / RANGE as f32, y as f32 / RANGE as f32);
+
+        ufmt::uwriteln!(
+            self.serial,
+            "setting relative motor speeds a={}, b={}",
+            x,
+            y
+        )
+        .ok();
+        self.stepper_a.set_speed(a);
+        self.stepper_b.set_speed(b);
+    }
+
+    fn tick(&mut self) {
+        self.stepper_a.step();
+        self.stepper_b.step();
     }
 }
 
-/// Computes the relative velocity of the paddle (X, Y), where X and Y are in the range [-1, 1].
-/// The speed is relative to the maximum speed of the motor.
-#[inline]
-const fn corexy_to_cartesian(mut x: f32, mut y: f32) -> (f32, f32) {
-    // If x + y > 1, scale the values such that x + y = 1.
-    if x + y > 1.0 {
-        let scale = 1.0 / (x + y);
-        x *= scale;
-        y *= scale;
+/// Receives all commands through the serial port.
+///
+/// # Serial Commands
+///
+/// Note: All integers are in little-endian format.
+///
+/// - ``V[X:i16][Y:i16]`` -> Set the velocity (relative speed) of the paddle to <X/10^4, Y/10^4>.
+///   Constraints: -10^4 <= X <= 10^4 and -10^4 <= Y <= 10^4.
+pub fn start_recv(hw: &mut Hardware) -> ! {
+    #[derive(Copy, Clone, Debug, PartialEq)]
+    enum Command {
+        V,
     }
 
-    // Define the two stepper motors as A and B, turning in the X direction clockwise.
-    // * If A = -X and B = X, the paddle moves kX in the +Y direction (where k is a constant)
-    // * If A = B = X, the paddle moves kX in the -X direction
-    //
-    // The speed of the motors is defined as:
-    // * A = (X + Y) * MAX_SPEED
-    // * B = (X - Y) * MAX_SPEED
-    //
-    // Reference: <http://wiki.fluidnc.com/en/config/kinematics>
-    let a = (x + y) * MOTOR_MAX_SPEED as f32;
-    let b = (x - y) * MOTOR_MAX_SPEED as f32;
-    (a, b)
+    impl Command {
+        const MAX_BUFFER_SIZE: usize = 4;
+
+        #[inline]
+        const fn from_u8(byte: u8) -> Option<Self> {
+            match byte {
+                b'V' => Some(Self::V),
+                _ => None,
+            }
+        }
+
+        #[inline]
+        const fn bytes_needed(&self) -> usize {
+            match self {
+                Command::V => 4,
+            }
+        }
+
+        fn done(&self, hw: &mut Hardware, bytes: &[u8]) {
+            match self {
+                Command::V => {
+                    let x = i16::from_le_bytes([bytes[0], bytes[1]]);
+                    let y = i16::from_le_bytes([bytes[2], bytes[3]]);
+                    hw.set_rel_speed(x, y);
+                }
+            }
+        }
+    }
+
+    let mut current = None::<Command>;
+    let mut buffer = [0_u8; Command::MAX_BUFFER_SIZE];
+    let mut buffer_idx = 0;
+
+    loop {
+        if let Ok(byte) = hw.serial.read() {
+            if let Some(cmd) = current {
+                buffer[buffer_idx] = byte;
+                buffer_idx += 1;
+                if buffer_idx == cmd.bytes_needed() {
+                    cmd.done(hw, &buffer);
+                    current = None;
+                    buffer_idx = 0;
+                }
+            } else {
+                current = Command::from_u8(byte);
+            }
+        }
+
+        hw.tick();
+    }
 }
 
 #[arduino_hal::entry]
@@ -71,39 +162,19 @@ fn main() -> ! {
     );
     ufmt::uwriteln!(serial, "Ready!").ok();
 
-    let mut stepper_a = Stepper::from_pins(
+    let stepper_a = Stepper::from_pins(
         pins.d2.into_output().downgrade(),
         pins.d5.into_output().downgrade(),
     );
-    let mut stepper_b = Stepper::from_pins(
+    let stepper_b = Stepper::from_pins(
         pins.d3.into_output().downgrade(),
         pins.d6.into_output().downgrade(),
     );
 
-    // Serial commands
-    // ===============
-    // NOTE: All integers are in little-endian format.
-    //
-    // - V[X:i32][Y:i32] -> Set the velocity (relative speed) of the paddle to <X/10^6, Y/10^6>.
-    //   Constraints: -10^6 <= X <= 10^6 and -10^6 <= Y <= 10^6.
-    loop {
-        match serial.read() {
-            Ok(b'V') => {
-                const RANGE: i32 = 1_000_000;
-
-                let x = serial.read_i32().clamp(-RANGE, RANGE);
-                let y = serial.read_i32().clamp(-RANGE, RANGE);
-                ufmt::uwriteln!(serial, "Received V({}, {})", x, y).ok();
-
-                let (a, b) = corexy_to_cartesian(x as f32 / RANGE as f32, y as f32 / RANGE as f32);
-                stepper_a.set_speed(a);
-                stepper_b.set_speed(b);
-            }
-            _ => {}
-        }
-
-        // Tick
-        stepper_a.step();
-        stepper_b.step();
-    }
+    let mut hw = Hardware {
+        serial,
+        stepper_a,
+        stepper_b,
+    };
+    start_recv(&mut hw)
 }
